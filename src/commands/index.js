02bce4d0c6056ef80584a7d5f736eda93c8aca9c
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-08-15 16:36:04 -0500

    Updated help command to encompass all commands for v1 release

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     team: require("./main/team"),
     help: require("./main/help"),
+    version: require("./main/version"),
     author: require("./main/author"),
 };
--- src/commands/main/help.js
+++ src/commands/main/help.js
@@ -5,17 +5,38 @@ module.exports = {
     description: "Shows all of the commands available",
     execute(msg, args) {
         const helpEmbed = new Discord.MessageEmbed()
-            // .setColor("")
             .setTitle("!in commands")
-            // .setThumbnail()
-            .addFields({
-                name: "!in help",
-                value: "Lists all of the commands available",
-            })
-            .setFooter(
-                "In-house-inator created by Riley Abrahamson",
-                "https://avatars3.githubusercontent.com/u/32375220?s=460&u=a375968682555bb66226556bf7ee33d6d35b7961&v=4"
+            .setThumbnail(
+                "https://raw.githubusercontent.com/RileyAbr/In-house-inator/master/assets/in-house-inator.jpg"
+            )
+            .setDescription(
+                `All commands begin with \`!in\`. Any command with \`{}\` indicates extra options for the command.`
             );
+        addFields({
+            name: "!in",
+            value: "Shorthand for !in team",
+        });
+        addFields({
+            name: "!in team {# teams} {# of players} {teamA, teamB, ...}",
+            value:
+                "Creates an in-house match. Defaults to 2 teams of 5. Can specifiy number of teams, players, and provide a list of team names.",
+        });
+        addFields({
+            name: "!in help",
+            value: "Lists all of the commands available",
+        });
+        addFields({
+            name: "!in version",
+            value: "Shows the current release",
+        });
+        addFields({
+            name: "!in author",
+            value: "Provides author and metadata for the bot",
+        });
+        setFooter(
+            "In-house-inator created by Riley Abrahamson",
+            "https://avatars3.githubusercontent.com/u/32375220?s=460&u=a375968682555bb66226556bf7ee33d6d35b7961&v=4"
+        );
 
         msg.channel.send(helpEmbed);
     },
