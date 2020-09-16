da983e2636ec931af2a38c7074b818cdbd62bcfa
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-16 18:49:11 -0500

    Re-ordered routes

--- in-house-inator-site/src/routes.jsx
+++ in-house-inator-site/src/routes.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import Content from "./components/Content";
 
 const routes = [
-    { path: "/", component: Content },
     { path: "/home", component: Content },
-    { path: "/:command", component: Content },
     { path: "/contact", component: Content },
+    { path: "/:command", component: Content },
+    { path: "/", component: Content },
 ];
 
 export default routes;
