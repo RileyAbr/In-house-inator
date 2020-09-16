e72fd864c4fce2abe5e222a64758af84f29ce65c
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-15 21:26:50 -0500

    Set up basic Credits tests

---
+++ in-house-inator-site/src/components/Credits/Credits.test.jsx
@@ -0,0 +1,21 @@
+// __tests__/fetch.test.js
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+beforeAll(() => render(<Credits />));
+
+test("Displays GitHub link", async () => {
+    const creditsLinks = screen.getAllByRole("link");
+
+    expect(creditsLinks[0]).toHaveTextContent("GitHub");
+    expect(creditsLinks[0]).toHaveAttribute(
+        "href",
+        "https://github.com/RileyAbr/In-house-inator"
+    );
+    expect(creditsLinks[1]).toHaveTextContent("Riley Abrahamson");
+    expect(creditsLinks[1]).toHaveAttribute(
+        "href",
+        "https://www.rileyabrahamson.com/"
+    );
+});
