0de21744bc91fc9ee81b91f4d403562c87810337
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-17 00:11:49 -0500

    Changed sidebar links to bulleted list

--- in-house-inator-site/src/components/Sidebar/Sidebar.jsx
+++ in-house-inator-site/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link as ReactRouterLink } from "react-router-dom";
 
 import logo from "../../in-house-inator_logo.png";
 
-import { Flex, Image, Heading, VStack } from "@chakra-ui/core";
+import {
+    Flex,
+    Image,
+    Heading,
+    VStack,
+    UnorderedList,
+    ListItem,
+    Link,
+} from "@chakra-ui/core";
 
 import Credits from "../Credits";
 
@@ -29,9 +37,23 @@ function Sidebar(props) {
         <VStack as="nav" height="100%" p={2}>
             <LogoMasthead />
 
-            {props.routes.map((route) => {
-                return <Link to={route.path}>{route.path}</Link>;
-            })}
+            {/* As of 9/17, the UnorderedList component from Chakra is broken and needs a bullet specified to be unordered */}
+            <UnorderedList
+                w="80%"
+                fontSize="xl"
+                listStyleType="disc"
+                color="scienceGreen"
+            >
+                {props.routes.map((route) => {
+                    return (
+                        <ListItem>
+                            <Link as={ReactRouterLink} to={route.path}>
+                                {route.path}
+                            </Link>
+                        </ListItem>
+                    );
+                })}
+            </UnorderedList>
             <Credits />
         </VStack>
     );
