fe8d03fb832bad0cb49030c607881f336fef0571
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-16 23:17:45 -0500

    Implemented responsive sidebar layout

--- in-house-inator-site/src/components/Sidebar/Sidebar.jsx
+++ in-house-inator-site/src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Heading } from "@chakra-ui/core";
+import logo from "../../logo.svg";
+
+import { Heading, VStack } from "@chakra-ui/core";
+
+import Credits from "../Credits";
 
 function Sidebar(props) {
     return (
-        <div>
+        <VStack as="nav" height="100%">
+            <img
+                width="50px"
+                height="50px"
+                src={logo}
+                className="App-logo"
+                alt="logo"
+            />
             <Heading color="evilPurple">Home</Heading>
             {props.routes.map((route) => {
                 return <Link to={route.path}>{route.path}</Link>;
             })}
             <hr />
-            {props.children}
-        </div>
+            <Credits />
+        </VStack>
     );
 }
 
