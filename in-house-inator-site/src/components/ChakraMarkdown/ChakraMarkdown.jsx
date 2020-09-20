209cf9c3d9c688441312834d9347443318a8610d
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-20 14:22:10 -0500

    Add image renderer to ChakraMarkdown

--- in-house-inator-site/src/components/ChakraMarkdown/ChakraMarkdown.jsx
+++ in-house-inator-site/src/components/ChakraMarkdown/ChakraMarkdown.jsx
@@ -13,12 +13,16 @@ const MarkdownCode = (props) => {
     return <CodeDisplay fontSize="lg">{props.children}</CodeDisplay>;
 };
 
+const MarkDownImage = (props) => {
+    return <Image py={3} src={props.src} alt={props.alt} />;
+};
+
 const ChakraMarkdown = (props) => {
     // These renderers control what components are rendered from the markdown file. By supplying it with Charka UI components, it will keep a consistant style with the rest of the page
     const chakraRenderers = {
-        image: Image,
         paragraph: MarkdownParagraph,
         inlineCode: MarkdownCode,
+        image: MarkDownImage,
     };
 
     return <ReactMarkdown source={props.source} renderers={chakraRenderers} />;
