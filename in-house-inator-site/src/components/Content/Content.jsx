33c01f6625f00aea1ab234baac0a8697b12853fb
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-19 23:06:22 -0500

    Created a method by which to parse the markdown files into the Content page

--- in-house-inator-site/src/components/Content/Content.jsx
+++ in-house-inator-site/src/components/Content/Content.jsx
@@ -1,16 +1,55 @@
 import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
+import { useParams } from "react-router-dom";
 
-import help from "../../commands_desc/help.md";
+import commands from "../../commands.json";
+
+const markDownFilenameParserRegex = new RegExp(/(^\.\/+|.md+$)/gm);
+
+const importAll = (r) => r.keys().map(r);
+const commandMarkdownFilesPaths = importAll(
+    require.context("../../commands_desc", false, /\.md$/)
+);
+
+// const commandMarkdownFilesPaths = require
+//     .context("../../commands_desc", false, /\.md$/)
+//     .keys();
+
+const commandMarkdownFileNames = require
+    .context("../../commands_desc", false, /\.md$/)
+    .keys()
+    .map((filePath) => {
+        return filePath.replace(markDownFilenameParserRegex, "");
+    });
+
+const commandMarkdownFilesMap = Object.assign(
+    ...commandMarkdownFileNames.map((v, i) => ({
+        [v]: commandMarkdownFilesPaths[i],
+    }))
+);
+
+console.log(commandMarkdownFilesPaths);
+console.log(commandMarkdownFileNames);
+console.log(commandMarkdownFilesMap);
 
 export default function Content() {
     const [commandDesc, setCommandDesc] = useState(null);
+    const commandParam = useParams().command; //If the user is on a command page, it is stored as a query parameter labeled {command: *}
+
+    console.log(commandMarkdownFilesMap[useParams().command]);
 
     useEffect(() => {
-        fetch(help)
-            .then((res) => res.text())
-            .then((text) => setCommandDesc(text));
-    });
+        if (
+            commands.includes(commandParam.toLowerCase()) &&
+            commandMarkdownFilesMap[commandParam]
+        ) {
+            fetch(commandMarkdownFilesMap[commandParam])
+                .then((res) => res.text())
+                .then((text) => setCommandDesc(text));
+        } else {
+            setCommandDesc("# Sorry! This command doesn't exist.");
+        }
+    }, [commandParam]);
 
     return (
         <>
