2293128d2e5428fad2ad076ea59552610d272cbc
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2020-09-20 00:46:42 -0500

    Update routes.js

--- in-house-inator-site/src/routes.js
+++ in-house-inator-site/src/routes.js
@@ -1,8 +1,6 @@
 import Home from "./components/Home";
 import Content from "./components/Content";
 
-import commands from "./commands.json";
-
 const routes = [
     { path: "/:command", component: Content },
     { path: "/", component: Home, exact: true },
