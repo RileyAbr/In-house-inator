f7935fe3b1dae9cbdfd4cd123697d750f673ccf7
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2023-02-04 19:49:54 -0800

    Add additional permissions for VoiceStates

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -11,6 +11,7 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 })
 
