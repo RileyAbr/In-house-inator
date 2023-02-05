02d8b0bbc90e64649a14cd441a0fc7512d7a6f45
RIley Abrahamson <32375220+RileyAbr@users.noreply.github.com>
2023-02-04 17:13:17 -0800

    Add infrastructure to track server count

--- bot/src/listeners/ready.ts
+++ bot/src/listeners/ready.ts
@@ -9,19 +9,22 @@ export default (client: Client): void => {
 
     await client.application.commands.set(Commands)
 
-    let isHelp = true
+    let guildCount = 0
+    await Promise.all(client.guilds.cache.map(async () => guildCount++))
+
+    let isHelpActivity = true
     client.user.setActivity('Try /help!')
 
     setInterval(() => {
-      if (isHelp) {
-        client?.user?.setActivity('Try /team!')
-        isHelp = false
+      if (isHelpActivity) {
+        client?.user?.setActivity(`in ${guildCount} servers!`)
+        isHelpActivity = false
       } else {
         client?.user?.setActivity('Try /help!')
-        isHelp = true
+        isHelpActivity = true
       }
-    }, 5000)
+    }, 14000)
 
-    console.info(`Logged in as ${client.user.tag}`)
+    console.info(`Logged in as ${client.user.tag} in ${guildCount} servers!`)
   })
 }
